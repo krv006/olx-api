8277c99d5139c1a0b91b12bae1f3a96217c0c817
krv006 <rvkamronbek@gmail.com>
2024-09-20 12:22:11 +0500

    models

--- apps/ads/models.py
+++ apps/ads/models.py
@@ -1,132 +1,113 @@
-# from django.db.models import ImageField, CASCADE, BooleanField, TextField, JSONField, OneToOneField, IntegerField, \
-#     CharField, DateTimeField, ForeignKey, Model, TextChoices
-# from django_ckeditor_5.fields import CKEditor5Field
-# from mptt.fields import TreeForeignKey
-# from mptt.models import MPTTModel
-#
-# # from shared.django.models import BaseSlugModel
-#
-# from django.db.models import Model, CharField, SlugField
-# from django.utils.text import slugify
-#
-#
-# class BaseSlugModel(Model):
-#     name = CharField(max_length=255)
-#     slug = SlugField(max_length=255, unique=True)
-#
-#     def save(self, *args, force_insert=False, force_update=False, using=None, update_fields=None):
-#         self.slug = slugify(self.name)
-#         super().save(*args, force_insert=force_insert, force_update=force_update, using=using,
-#                      update_fields=update_fields)
-#
-#     class Meta:
-#         abstract = True
-#
-#     def __str__(self):
-#         return self.slug
-#
-#
-# class Region(Model):
-#     name = CharField(max_length=50)
-#
-#
-# class District(Model):
-#     name = CharField(max_length=50)
-#     region = ForeignKey('ads.Region', CASCADE)
-#
-#
-# class Category(BaseSlugModel, MPTTModel):
-#     image = ImageField(upload_to='category/', null=True, blank=True)
-#     parent = TreeForeignKey('self', CASCADE, null=True, blank=True, related_name='children')
-#
-#
-# class ExtraFields(Model):
-#     category = OneToOneField('ads.Category', CASCADE)
-#     fields = JSONField(default=dict)
-#
-#
-# class AdvertisementImage(Model):
-#     advert = ForeignKey('ads.Advert', CASCADE, related_name='images')
-#     image = ImageField(upload_to='product/')
-#
-#
-# class FavoriteAdvertisement(Model):
-#     advert = ForeignKey('ads.Advert', CASCADE, related_name='favorite')
-#     user = ForeignKey('users.User', CASCADE, related_name='favorite')
-#
-#
-# class Message(Model):
-#     advert = ForeignKey('ads.Advert', CASCADE)
-#     owner = ForeignKey('users.User', CASCADE)
-#     text = TextField()
-#
-#
-# class Currency(Model):
-#     name = CharField(max_length=25)
-#
-#
-# class Advert(BaseSlugModel):
-#     class PaymentType(TextChoices):
-#         CASH = 'cash', 'Cash'
-#         FREE = 'free', 'Free'
-#         EXCHANGE = 'exchange', 'Exchange'
-#
-#     class Status(TextChoices):
-#         ACTIVE = 'active', 'Active'
-#         WAITING = 'waiting', 'Waiting'
-#         UNPAID = 'unpaid', 'Unpaid'
-#         INACTIVE = 'inactive', 'Inactive'
-#         REJECTED = 'rejected', 'Rejected'
-#
-#     status = CharField(max_length=20, choices=Status.choices, default=Status.WAITING)
-#     category = ForeignKey('ads.Category', CASCADE)
-#     price_type = CharField(max_length=10, choices=PaymentType.choices, default=PaymentType.CASH)
-#     currency = ForeignKey('ads.Currency', CASCADE, null=True, blank=True)
-#     price = IntegerField(null=True, blank=True)
-#     description = TextField()
-#     owner = ForeignKey('users.User', CASCADE)
-#     city = ForeignKey('ads.District', CASCADE)
-#     view_count = IntegerField(db_default=0)
-#     is_new = BooleanField(default=False)
-#     extra_filed_info = JSONField()
-#
-#     is_business = BooleanField(default=False)
-#     contact = JSONField(default=dict)
-#     updated_at = DateTimeField(auto_now_add=True)
-#     created_at = DateTimeField(auto_now=True)
-#     auto_renewal = BooleanField(db_default=False)
-#
-#
-# class WorkAds(Model):
-#     class WorkType(TextChoices):
-#         PERMANENTLY = 'permanent', 'Permanent'
-#         TEMPORARY = 'temporary', 'Temporary'
-#
-#     class TypeEmployment(TextChoices):
-#         FULL_TIME = 'full time', 'Full time'
-#         INCOMPLETE = 'incomplete', 'Incomplete'
-#
-#     min_price = IntegerField()
-#     max_price = IntegerField()
-#     category = ForeignKey('ads.Category', CASCADE)
-#     contact = JSONField(default=dict)
-#     description = CKEditor5Field()
-#     title = CharField(max_length=255)
-#     work_type = CharField(max_length=25, choices=WorkType.choices)
-#     type_employment = CharField(max_length=25, choices=TypeEmployment.choices)
-#     updated_at = DateTimeField(auto_now_add=True)
-#     created_at = DateTimeField(auto_now=True)
-#     owner = ForeignKey('users.User', CASCADE)
-#     city = ForeignKey('ads.District', CASCADE)
-#     auto_renewal = BooleanField(default=False)
-#     extra_filed_info = JSONField()
-#
-#
-# class ProductHistory(Model):
-#     product_id = IntegerField()
-#     product_name = CharField(max_length=255)
-#     price = IntegerField()
-#     deleted_at = DateTimeField(auto_now=True)
+from django.db.models import ImageField, CASCADE, BooleanField, TextField, JSONField, OneToOneField, IntegerField, \
+    DateTimeField, ForeignKey, Model, TextChoices
+from django.db.models import Model, CharField
+from django_ckeditor_5.fields import CKEditor5Field
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+from shared.django.model import BaseSlugModel
+
+
+class Region(Model):
+    name = CharField(max_length=50)
+
+
+class District(Model):
+    name = CharField(max_length=50)
+    region = ForeignKey('ads.Region', CASCADE)
+
+
+class Category(BaseSlugModel, MPTTModel):
+    image = ImageField(upload_to='category/', null=True, blank=True)
+    parent = TreeForeignKey('self', CASCADE, null=True, blank=True, related_name='children')
+
+
+class ExtraFields(Model):
+    category = OneToOneField('ads.Category', CASCADE)
+    fields = JSONField(default=dict)
+
+
+class AdvertisementImage(Model):
+    advert = ForeignKey('ads.Advert', CASCADE, related_name='images')
+    image = ImageField(upload_to='product/')
+
+
+class FavoriteAdvertisement(Model):
+    advert = ForeignKey('ads.Advert', CASCADE, related_name='favorite')
+    user = ForeignKey('users.User', CASCADE, related_name='favorite')
+
+
+class Message(Model):
+    advert = ForeignKey('ads.Advert', CASCADE)
+    owner = ForeignKey('users.User', CASCADE)
+    text = TextField()
+
+
+class Currency(Model):
+    name = CharField(max_length=25)
+
+
+class Advert(BaseSlugModel):
+    class PaymentType(TextChoices):
+        CASH = 'cash', 'Cash'
+        FREE = 'free', 'Free'
+        EXCHANGE = 'exchange', 'Exchange'
+
+    class Status(TextChoices):
+        ACTIVE = 'active', 'Active'
+        WAITING = 'waiting', 'Waiting'
+        UNPAID = 'unpaid', 'Unpaid'
+        INACTIVE = 'inactive', 'Inactive'
+        REJECTED = 'rejected', 'Rejected'
+
+    status = CharField(max_length=20, choices=Status.choices, default=Status.WAITING)
+    category = ForeignKey('ads.Category', CASCADE)
+    price_type = CharField(max_length=10, choices=PaymentType.choices, default=PaymentType.CASH)
+    currency = ForeignKey('ads.Currency', CASCADE, null=True, blank=True)
+    price = IntegerField(null=True, blank=True)
+    description = TextField()
+    owner = ForeignKey('users.User', CASCADE)
+    city = ForeignKey('ads.District', CASCADE)
+    view_count = IntegerField(db_default=0)
+    is_new = BooleanField(default=False)
+    extra_filed_info = JSONField()
+
+    is_business = BooleanField(default=False)
+    contact = JSONField(default=dict)
+    updated_at = DateTimeField(auto_now_add=True)
+    created_at = DateTimeField(auto_now=True)
+    auto_renewal = BooleanField(db_default=False)
+
+
+class WorkAds(Model):
+    class WorkType(TextChoices):
+        PERMANENTLY = 'permanent', 'Permanent'
+        TEMPORARY = 'temporary', 'Temporary'
+
+    class TypeEmployment(TextChoices):
+        FULL_TIME = 'full time', 'Full time'
+        INCOMPLETE = 'incomplete', 'Incomplete'
+
+    min_price = IntegerField()
+    max_price = IntegerField()
+    category = ForeignKey('ads.Category', CASCADE)
+    contact = JSONField(default=dict)
+    description = CKEditor5Field()
+    title = CharField(max_length=255)
+    work_type = CharField(max_length=25, choices=WorkType.choices)
+    type_employment = CharField(max_length=25, choices=TypeEmployment.choices)
+    updated_at = DateTimeField(auto_now_add=True)
+    created_at = DateTimeField(auto_now=True)
+    owner = ForeignKey('users.User', CASCADE)
+    city = ForeignKey('ads.District', CASCADE)
+    auto_renewal = BooleanField(default=False)
+    extra_filed_info = JSONField()
+
+
+class ProductHistory(Model):
+    product_id = IntegerField()
+    product_name = CharField(max_length=255)
+    price = IntegerField()
+    deleted_at = DateTimeField(auto_now=True)
 
 
 # from django.db.models import Model, CharField
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,103 @@
-from django.db import models
 
-# Create your models here.
+import uuid
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import UserManager, PermissionsMixin
+from django.core.validators import FileExtensionValidator
+from django.db.models import BooleanField, CharField, UUIDField, DateTimeField, ImageField, TextChoices, Model, \
+    ForeignKey, CASCADE, EmailField, FileField, DateField, TextField, OneToOneField
+from django.utils import timezone
+
+# from users.filters import validate_file_size
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    class SocialNetworkAccountType(TextChoices):
+        GOOGLE = 'google', 'Google'
+        FACEBOOK = 'facebook', 'Facebook'
+
+    uuid = UUIDField(default=uuid.uuid4, unique=True)
+    username = CharField(max_length=25, null=True, blank=True)
+    phone_number = CharField(max_length=20, unique=True)
+    is_business = BooleanField(default=False)
+    city = ForeignKey('ads.District', CASCADE, blank=True, null=True)
+    first_name = CharField(max_length=30, null=True, blank=True)
+    last_name = CharField(max_length=30, null=True, blank=True)
+
+    photo = ImageField(upload_to='users1/', default='d/image.png')
+    social_network_account_type = CharField(max_length=20, choices=SocialNetworkAccountType.choices, null=True)
+    is_online = BooleanField(null=True, blank=True)
+
+    email = EmailField(null=True, blank=True)
+    is_staff = BooleanField(default=False)
+    is_active = BooleanField(default=True)
+    created_at = DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['username']
+
+
+class CandidateProfile(Model):
+    class Drive(TextChoices):
+        A = 'a', 'A'
+        B = 'b', 'B'
+        C = 'c', 'C'
+        D = 'd', 'D'
+        BE = 'be', 'BE'
+        CE = 'ce', 'CE'
+        DE = 'de', 'DE'
+
+    # desired_job_title = CharField(max_length=255,
+    #                               help_text='Vergul bilan ajratib, tanlangan lavozimlarni kiriting. 15 tagacha qoʻshishingiz mumkin.',
+    #                               verbose_name='Istalgan lavozim nomi')
+    # desired_city = ForeignKey('ads.District', CASCADE, verbose_name='Istalgan hudud')
+    # cv = FileField(upload_to='user_cv/',
+    #                help_text="Siz shu formatlarda fayl yuklashingiz mumkin: pdf, doc, docx, odt. Maksimal o'lchami 4MB.",
+    #                validators=[FileExtensionValidator(allowed_extensions=['pdf', 'doc', 'docx', 'odt']),
+    #                            validate_file_size])
+    owner = OneToOneField('users.User', CASCADE)
+    first_name = CharField(max_length=50)
+    last_name = CharField(max_length=50)
+    phone_number = CharField(max_length=15, unique=True)
+    email = EmailField(null=True, blank=True)
+    year_of_birth = CharField(max_length=4)
+    name_of_educational = CharField(max_length=255, verbose_name="Ta'lim muassasasi nomi *")
+    direction = CharField(max_length=255, verbose_name="Yo'nalish / Ixtisos / egallangan kasb", null=True, blank=True)
+    year_of_admission = CharField(max_length=4, verbose_name='Qabul qilingan yil *')
+    graduation_year = DateField(verbose_name="Bitiruv yili *", null=True, blank=True)
+    studying_now = BooleanField(default=False, null=True, blank=True)
+    position_name = CharField(max_length=255, verbose_name="Lavozim nomi *")
+    employer = CharField(max_length=255, verbose_name="Ish beruvchi", null=True, blank=True)
+    work_start_date = DateField(verbose_name="Boshlanish vaqti")
+    work_end_date = DateField(verbose_name="Tugash muddati", null=True, blank=True)
+    working_now = BooleanField(default=False, null=True, blank=True)
+    job_duties = TextField(max_length=1500, verbose_name="Shu lavozimdagi majburiyatlarning tavsifi", null=True,
+                           blank=True)
+    drivers_license = CharField(max_length=20, verbose_name="Toifani tanlash yoki qo'shish", choices=Drive.choices)
+    hobby = CharField(max_length=30, verbose_name="Xobbiyingiz")
+
+
+class Language(Model):
+    class Language(TextChoices):
+        ENGLISH = 'english', 'English'
+        UZBEK = 'uzbek', 'Uzbek'
+        RUSSIAN = 'russian', 'Russian'
+
+    class LanguageLevel(TextChoices):
+        ADVANCED = 'advanced', 'Advanced'
+        PRIMARY = 'primary', 'Primary'
+        LIBERAL = 'liberal', 'Liberal'
+        MEDIUM = 'medium', 'Medium'
+
+    candidate_profile = ForeignKey('users.CandidateProfile', CASCADE)
+    language = CharField(max_length=30, choices=Language.choices, verbose_name='Til')
+    level_knowledge = CharField(max_length=30, verbose_name="Bilim darajasi", choices=LanguageLevel.choices)
+
+
+class RecentlyViewedAds(Model):
+    user = ForeignKey('users.User', CASCADE)
+    ads = ForeignKey('ads.Advert', CASCADE)
+    created_at = DateTimeField(auto_now=True)
